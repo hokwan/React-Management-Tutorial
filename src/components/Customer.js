3ee25d38846e0021920fd8b2bb3f1b7a54e71052
Hokwan <hokwani@naver.com>
2024-09-24 15:50:33 +0900

    Create React Project

---
+++ src/components/Customer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Customer extends React.Component {
+    render() {
+        return (
+            <div>
+            <h2>호이킨</h2>
+            <p>961222</p>
+            <p>Mail</p>
+            <p>팀장</p>
+        </div>
+        )
+        
+    }
+}
+export default Customer;
